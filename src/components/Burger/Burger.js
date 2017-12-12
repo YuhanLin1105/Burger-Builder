2975a77dcecf098c31e1f7f6ea3aa287a873c042
Yuhan Lin <yuhanlin1105@gmail.com>
2017-12-12 15:58:07 -0500

    Add BuildControls

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,23 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 
 const burger = (props)=>{
+    let tansIngredients = Object.keys(props.ingredients)
+        .map(igKey=>{
+            return [...Array(props.ingredients[igKey])]
+                .map((_, i)=>{
+                    return <BurgerIngredient key={igKey+i} type={igKey}/>;
+                });
+        })
+        .reduce((pre,cur)=>{
+            return pre.concat(cur);
+        },[]);
+    if (tansIngredients.length===0)
+        tansIngredients="Please add some ingredients."
+
     return(
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
+            {tansIngredients}
             <BurgerIngredient type="bread-bottom"/>
         </div>
     );
