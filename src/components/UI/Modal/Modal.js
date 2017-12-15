535a90c4ac11ac195b33cdd1538d430f809c3f03
Yuhan Lin <yuhanlin1105@gmail.com>
2017-12-15 13:37:33 -0500

    add error handler

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Modal.css';
 import Ax from '../../../hoc/Ax';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => (
-    <Ax>
-        <Backdrop show={props.show} clicked={props.modalClosed}/>
-        <div className={classes.Modal}
-            style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show ? '1' : '0'
-            }}>
-            {props.children}
-        </div>        
-    </Ax>
-);
+class Modal extends Component {
 
-export default modal;
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
+    }
+
+    render() {
+        return (
+            <Ax>
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+                <div className={classes.Modal}
+                    style={{
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.show ? '1' : '0'
+                    }}>
+                    {this.props.children}
+                </div>
+            </Ax>
+        )
+    }
+}
+
+
+export default Modal;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,6 +6,7 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 import Modal from '../../components/UI/Modal/Modal'
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
 import axios from '../../axios-orders';
+import Spinner from '../../components/UI/Spinner/Spinner'
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -24,42 +25,55 @@ class BurgerBuilder extends Component {
             cheese: 0,
             meat: 0
         },
-        purchasing:false,
-        totalPrice: 4
+        purchasing: false,
+        totalPrice: 4,
+        loading: false
     }
 
-    purchaseContinueHandler = () =>{
+    purchaseContinueHandler = () => {
+        this.setState({
+            loading:true
+        });
+
         const order = {
-            ingredients:this.state.ingredients,
+            ingredients: this.state.ingredients,
             price: this.state.totalPrice,
-            customer:{
-                name:"Lin",
-                address:{
-                    street:"aaa",
-                    zipCode:"1234",
-                    country:'US'
+            customer: {
+                name: "Lin",
+                address: {
+                    street: "aaa",
+                    zipCode: "1234",
+                    country: 'US'
                 },
                 email: 'test@test.com',
-                deliveryMethod:'fastest'
+                deliveryMethod: 'fastest'
             }
-            
+
         }
         axios.post('/order.json', order)
-            .then(response=>console.log(response))
-            .catch(error=>console.log(error));
+            .then(response =>{
+                this.setState({
+                    loading:false,
+                    purchasing:false});
+            })            
+            .catch(error =>{
+                this.setState({
+                    loading:false,
+                    purchasing:false});
+            });    
     }
 
 
-    purchaseCancelHandler = ()=>{
+    purchaseCancelHandler = () => {
         this.setState({
-            purchasing:false
+            purchasing: false
         });
-        
+
     }
 
-    purchaseHandler=()=>{
+    purchaseHandler = () => {
         this.setState({
-            purchasing:true
+            purchasing: true
         });
     }
 
@@ -118,15 +132,21 @@ class BurgerBuilder extends Component {
 
     render() {
         const disableInfo = this.checkDisableInfo();
+
+        let orderSummary = <OrderSummary
+            price={this.state.totalPrice}
+            ingredients={this.state.ingredients}
+            btnCancel={this.purchaseCancelHandler}
+            btnContinue={this.purchaseContinueHandler} />;
+        if(this.state.loading){
+            orderSummary =<Spinner></Spinner>;
+        }
+
         return (
             <Ax>
                 <Modal show={this.state.purchasing}
                     modalClosed={this.purchaseCancelHandler}>
-                    <OrderSummary 
-                        price={this.state.totalPrice}
-                        ingredients={this.state.ingredients}
-                        btnCancel={this.purchaseCancelHandler}
-                        btnContinue={this.purchaseContinueHandler}/>
+                    {orderSummary}
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls
