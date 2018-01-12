e39b559fd779fc79c808e3a020e14571afdbf523
Yuhan Lin <yuhanlin1105@gmail.com>
2018-01-11 20:13:48 -0500

    merge

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -85,24 +85,6 @@ class ContactData extends Component {
 
     }
 
-    checkValidity(value, rule){
-        let isValid=true;
-        
-        if(rule.required){
-            isValid = value.trim() !== ''&& isValid;
-        }
-
-        if(rule.minLength){
-            isValid = value.length>= rule.minLength && isValid;
-        }
-
-        if(rule.maxLength){
-            isValid = value.length<= rule.maxLength && isValid;
-        }
-
-        return isValid;
-    }
-
     orderHandler = (event) => {
         event.preventDefault();
         this.setState({
